974b189da8efd59a4e7557714798f17b9e60c93e
BuildTools <unconfigured@null.spigotmc.org>
2022-04-16 23:26:21 +0200

    xd

--- webscrapper-front/src/App.js
+++ webscrapper-front/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to refactor.
+          Edit <code>src/App.js</code> and save to xd.
         </p>
         <a
           className="App-link"
