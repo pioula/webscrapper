961beda959a57aa3f773984a9c67c5b02e04cef1
BuildTools <unconfigured@null.spigotmc.org>
2022-04-18 17:42:59 +0200

    tidier

--- webscrapper-front/src/App.js
+++ webscrapper-front/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tidy
+          Tidier
         </p>
         <a
           className="App-link"
