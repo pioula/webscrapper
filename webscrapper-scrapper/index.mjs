534e9aac629b925a962a70bef7956aff5644765a
BuildTools <unconfigured@null.spigotmc.org>
2022-04-18 18:44:26 +0200

    simple

--- webscrapper-scrapper/index.mjs
+++ webscrapper-scrapper/index.mjs
@@ -4,7 +4,7 @@ const app = express();
 const port = 3001;
 
 app.use((req, res) => {
-    res.status(200).send(`<div>Hi! ${process.env.DB_USER}</div>`);
+    res.status(200).send(`<div>Hi! Wassup!</div>`);
     res.end();
 });
 
